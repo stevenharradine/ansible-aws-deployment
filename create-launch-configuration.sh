5c7db44aec155dd58c1f465c4ba8cfcab4ceb8e2
Steven Harradine <stevenharradine@gmail.com>
2016-01-11 17:39:31 -0500

    get and populate security groups for launch configuration

--- create-launch-configuration.sh
+++ create-launch-configuration.sh
@@ -1,8 +1,8 @@
 printf "create launch configuration ($launch_configuration_name)"
 launch_configuration=`aws autoscaling create-launch-configuration \
-  --launch-configuration-name=$launch_configuration_name \
-  --image-id=$image_id \
-  --instance-type=$instance_type \
+  --launch-configuration-name $launch_configuration_name \
+  --image-id $image_id \
+  --instance-type $instance_type \
   --associate-public-ip-address \
-  --security-groups=$security_group_id`
+  --security-groups $security_group_ids`
 echo " . done"
--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,11 @@
 
 source config.sh
 
-# HACKS
-image_id=ami-40fde621
-security_group_id=sg-2566e541
-
 source vars.sh
 
 source deploy-app-code.sh
 source create-ami.sh
+source get-security-group-id.sh
 source create-launch-configuration.sh
 source use-new-launch-configuration.sh
 source scale-up.sh
