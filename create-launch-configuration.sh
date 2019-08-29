9c1d790587998aa3c283ef5bcd6763c4909979cd
Steven Harradine <stevenharradine@gmail.com>
2019-08-29 08:12:39 -0400

    execute launch config seperately from drafting it so i can conditionally modify it

--- create-launch-configuration.sh
+++ create-launch-configuration.sh
@@ -1,9 +1,11 @@
 printf "create launch configuration ($launch_configuration_name)"
-launch_configuration=`aws autoscaling create-launch-configuration \
+launch_configuration="aws autoscaling create-launch-configuration \
   --region $region \
   --launch-configuration-name $launch_configuration_name \
   --image-id $image_id \
   --instance-type $instance_type \
   --associate-public-ip-address \
-  --security-groups $security_group_ids`
+  --security-groups $security_group_ids"
+
+ `$launch_configuration`
 echo " . done"
