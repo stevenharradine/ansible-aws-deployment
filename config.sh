499a94d3bf0310d846a3a3ef586e81d9b1f8787b
Steven Harradine <stevenharradine@gmail.com>
2019-08-29 08:22:13 -0400

    scale to different sizes based on the environment tier

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 instance_id=$1
 
 domain=teluswebteam.com
-autoscaling_group_desire_capacity=1
 aws_regions=(us-west-2 us-west-1 us-east-1)
+autoscaling_group_desired_capacity_production=6
+autoscaling_group_desired_capacity_staging=3
--- vars.sh
+++ vars.sh
@@ -3,5 +3,10 @@ name=$project-$role-$timestamp
 launch_configuration_name=$name
 security_group_name=$project-$role
 autoscaling_group_name=$project-$role
+if [[ "$environment_tier" == "production" ]]; then
+	autoscaling_group_desired_capacity=$autoscaling_group_desired_capacity_production
+else
+	autoscaling_group_desired_capacity=$autoscaling_group_desired_capacity_staging
+fi
 autoscaling_group_desired_capacity_double=$(($autoscaling_group_desired_capacity*2))
 spot_price=0.044
